a03ec69fa88064532d1054af00431965a5926960
tomsontai <ktai8@my.bcit.ca>
2019-10-31 16:02:54 -0700

    added a comment to test server

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let express = require("express");
 const app = express();
 
 let bodyParser = require('body-parser');
-
+// Use port from env or 3000
 const port = process.env.PORT ||  3000;
 
 app.use('/', express.static(__dirname));
